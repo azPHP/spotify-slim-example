9534ea7653b322200dc913ee2bab02e074fd0f52
Jeremy Giberson <jeremyg@webpt.com>
2016-03-20 16:08:17 -0700

    grouped spotify routes and applied authentication only to spotify module

--- src/middleware.php
+++ src/middleware.php
@@ -1,8 +1,5 @@
 <?php
 // Application middleware
-use Psr7Middlewares\Middleware\BasicAuthentication;
 use Psr7Middlewares\Middleware\ResponseTime;
-
-$app->add(new BasicAuthentication(['azphp' => 'group']));
 
 $app->add(new ResponseTime);
--- src/routes.php
+++ src/routes.php
@@ -1,27 +1,34 @@
 <?php
 // Routes
+use Psr7Middlewares\Middleware\BasicAuthentication;
+use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+$app->group('/spotify', function() {
+    /** @var App $app */
+    $app = $this;
 
-$app->map(['GET','POST'], '/spotify', function(Request $request, Response $response, $args) {
-    $data = ['albums' => null];
+    $app->map(['GET','POST'], '', function(Request $request, Response $response, $args) {
+        $data = ['albums' => null];
 
-    if($request->isPost() && $search = $request->getParam('search'))
-    {
-        $data['albums'] = $this->albums->search($search);
-    }
+        if($request->isPost() && $search = $request->getParam('search'))
+        {
+            $data['albums'] = $this->albums->search($search);
+        }
 
-    return $this->renderer->render($response, 'spotify.phtml', $data);
-});
+        return $this->renderer->render($response, 'spotify.phtml', $data);
+    });
 
-$app->get('/spotify/album/{id}', function(Request $request, Response $response, $args) {
-    $id = $args['id'];
+    $app->get('/album/{id}', function(Request $request, Response $response, $args) {
+        $id = $args['id'];
 
-    $album = $this->albums->getAlbum($id);
+        $album = $this->albums->getAlbum($id);
 
-    return $this->renderer->render($response, 'album.phtml', ['album' => $album]);
-});
+        return $this->renderer->render($response, 'album.phtml', ['album' => $album]);
+    });
+})->add(new BasicAuthentication(['azphp' => 'group']));
+
 
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
